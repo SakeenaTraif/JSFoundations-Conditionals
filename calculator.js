4f38dfb6712e593ea7ea1e4702b6bd3da19a6de1
SakeenaTraif <s.traif30@gmail.com>
2021-01-10 15:28:45 +0300

    Conditional EX

--- calculator.js
+++ calculator.js
@@ -1,3 +1,19 @@
 // Define the numbers and the operation here
+let num1=5;
+let num2=3;
+let operation="*";
 
 // Write if-statements here
+
+if(operation === "+"){
+    console.log(`Addtion is: ${num1+num2}`);
+}
+else if(operation === "-"){
+    console.log(`Subtract is: ${num1-num2}`);
+}
+else if(operation === "*"){
+    console.log(`Multiplication is: ${num1*num2}`);
+}
+else if(operation === "/"){
+    console.log(`Divition is: ${num1/num2}`);
+}
