e9219cb093360a477543276d693a5d7e53c11a2c
SakeenaTraif <s.traif30@gmail.com>
2021-01-10 16:50:35 +0300

    Conditional EX

--- calculator.js
+++ calculator.js
@@ -3,6 +3,7 @@ let num1=5;
 let num2=3;
 let operation="*";
 
+
 // Write if-statements here
 
 if(operation === "+"){
@@ -17,3 +18,17 @@ else if(operation === "*"){
 else if(operation === "/"){
     console.log(`Divition is: ${num1/num2}`);
 }
+
+// Using promt Function 
+
+/*let num1= parseInt(prompt("Enter First number :"));
+let num2= parseInt(prompt("Enter Second number :"));
+let operation= prompt("Enter Operation :");
+
+
+// Write if-statements here
+
+if(operation === "+") console.log(`Addtion is: ${num1+num2}`);
+else if(operation === "-") console.log(`Subtract is: ${num1-num2}`);
+else if(operation === "*") console.log(`Multiplication is: ${num1*num2}`);
+else if(operation === "/") console.log(`Divition is: ${num1/num2}`);*/
